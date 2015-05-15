285f860619331694a0e77ad149ef31a6373c6059
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-14 20:34:27 -0400

    sdi-Lab5 Adding Function Progress

---
+++ sdi-lab5/Functions/Adding Function.js
@@ -0,0 +1,21 @@
+/*
+ Fernando Luis Aguiar Guevarez
+ Lab 5 Functions
+ 05/14/15
+ SDI
+ */
+
+//alert("JavaScript works!");
+
+//// Global Variables
+//
+//var operationResult;
+//
+//// Functions
+//
+//function addingFunction(addNum1, addNum2)
+//{
+//    //Addition Function
+//    operationResult = (addNum1 + addNum2);
+//    return operationResult;
+//}
