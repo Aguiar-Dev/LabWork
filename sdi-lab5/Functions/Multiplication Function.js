933d65f41328a8ac02b4f6cdb4a05231b6dae259
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-14 20:35:26 -0400

    sdi-Lab5 Multiplication Function

---
+++ sdi-lab5/Functions/Multiplication Function.js
@@ -0,0 +1,35 @@
+/*
+ Fernando Luis Aguiar Guevarez
+ Lab 5 Functions
+ 05/14/15
+ SDI
+ */
+
+//alert("JavaScript works!");
+
+// Global Variables
+
+//var operationResult;
+//
+//// Functions
+//
+//function addingFunction(addNum1, addNum2)
+//{
+//    //Addition Function
+//    operationResult = (addNum1 + addNum2);
+//    return operationResult;
+//}
+//
+//function subtractingFunction(subtractNum1, subtractNum2)
+//{
+//    //Subtraction Function
+//    operationResult = (subtractNum1 - subtractNum2);
+//    return operationResult;
+//}
+//
+//function multiplicationFunction(multiplyNum1, multiplyNum2)
+//{
+//    //Multiplication Function
+//    operationResult = (multiplyNum1 * multiplyNum2);
+//    return operationResult;
+//}
