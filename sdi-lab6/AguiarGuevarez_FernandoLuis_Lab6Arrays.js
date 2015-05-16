d3d3f3a2fd0679b3d4d0018252f87c461e3c79ec
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-16 14:17:51 -0400

    Lab 6 Progress 1
    Create Function.

---
+++ sdi-lab6/AguiarGuevarez_FernandoLuis_Lab6Arrays.js
@@ -0,0 +1,20 @@
+/*
+    Fernando Luis Aguiar Guevarez
+    SDI - Lab 6
+    Working with Arrays
+    05 / 16 / 15
+ */
+//alert("JavaScript works!");
+
+// Functions
+
+function familyFunction()
+{
+    var family = ["Fernando", "Maritza", "Irmarie", "Nicolle", "Jakob", "Camila"];
+    var descriptions = ["Father", "Mother", "Oldest Sister", "Older Sister", "Nephew", "Niece"];
+}
+
+// Main Code
+
+familyFunction();
+
