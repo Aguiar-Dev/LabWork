d558cabb940e5e709d06b38eb9f6a94780812135
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-19 13:06:58 -0400

    Lab 7 Progress

---
+++ sdi-lab7/AguiarGuevarez_FernandoLuis_Lab7.js
@@ -0,0 +1,24 @@
+/*
+    Fernando L. Aguiar
+    sdi-lab 7
+    Loops, Functions, and Arrays
+    05/19/15
+ */
+
+//alert("JavaScript works!");
+
+//Global Variables
+
+var array1 = [5, 2, 8, 4, 9, 1];
+var array2 = [15, 28, 11, 24, 16, 21, 30, 10];
+var array3 = [35, 50, 42, 39, 65, 90, 71, 100, 48, 86, 55, 82];
+
+//Function
+
+function bubbleFunction(accArray)
+{
+
+
+
+
+}
