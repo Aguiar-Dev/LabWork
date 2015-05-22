693d7586b007f3bf003e2c0218c7b3bea179eaa0
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-21 20:48:39 -0400

    Lab 8 Problem 1
    Progress 1

---
+++ sdi-lab8/AguiarGuevarez_FernandoLuis_Lab8.js
@@ -0,0 +1,28 @@
+/*
+    Fernando Luis Aguiar Guevarez
+    sdi-Lab 8
+    05/21/15
+ */
+
+//alert("JavaScript works!");
+
+//Problem 1
+
+ //global variables
+
+var emailPrompt;
+
+ function
+
+function checkEmail(email)
+{
+
+}
+
+ //main code
+
+emailPrompt = prompt("Please enter an email:");
+
+checkEmail(emailPrompt);
+
+
