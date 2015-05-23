4a241470c76374d914372e294f4ea3462a53ffda
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-23 14:18:23 -0400

    Lab 9 Problem 1
    Progress part 1

---
+++ sdi-lab9/AguiarGuevarez_FernandoLuis_Lab9Methods.js
@@ -0,0 +1,25 @@
+/*
+    Fernando Luis Aguiar Guevarez
+    sdi-Lab 9
+    Number, Math, and Date Methods
+    05/23/15
+ */
+
+//alert("JavaScript works!");
+
+////Problem 1
+
+//global variables
+
+var formatNumber;
+var decimalNumber;
+var returnedNumber;
+
+//function
+function decimalFormat(userNumber, decimalFormater)
+{
+}
+
+
+
+
