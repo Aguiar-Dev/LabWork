3d3bc3b8e2c52862f7bc3b10dabafb75d350e14a
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-23 21:13:49 -0400

    Lab 9 Problem 2
    Progress part 1

--- sdi-lab9/AguiarGuevarez_FernandoLuis_Lab9Methods.js
+++ sdi-lab9/AguiarGuevarez_FernandoLuis_Lab9Methods.js
@@ -11,23 +11,40 @@
 
 //global variables
 
-var formatNumber;
-var decimalNumber;
-var returnedNumber;
+//var formatNumber;
+//var decimalNumber;
+//var returnedNumber;
+//
+////function
+//function decimalFormat(userNumber, decimalFormater)
+//{
+//    newNumber = userNumber.toFixed(decimalFormater);
+//    return newNumber;
+//}
+//
+////main code
+//formatNumber = parseFloat(prompt("Please enter a number to format:", "Type in here"));
+//decimalNumber = parseInt(prompt("Please enter the amount of decimal places you want:", "Type in here"));
+//
+//returnedNumber = decimalFormat(formatNumber, decimalNumber);
+//
+//console.log("You entered the number " + formatNumber + " and it was formatted into " + returnedNumber + ".");
+
+
+
+////Problem 2
+
+//global variables
+
+var stringNumber;
+var numberReturn;
 
 //function
-function decimalFormat(userNumber, decimalFormater)
+function givingNumber(userNumber)
 {
-    newNumber = userNumber.toFixed(decimalFormater);
-    return newNumber;
+
 }
 
 //main code
-formatNumber = parseFloat(prompt("Please enter a number to format:", "Type in here"));
-decimalNumber = parseInt(prompt("Please enter the amount of decimal places you want:", "Type in here"));
-
-returnedNumber = decimalFormat(formatNumber, decimalNumber);
-
-console.log("You entered the number " + formatNumber + " and it was formatted into " + returnedNumber + ".");
 
 
