f93597a82b153a684d0538e8ac6273a65428f503
FernandoAguiarGuevarez <flaguiarguevarez@fullsail.edu>
2015-05-26 21:08:25 -0400

    Lab 10 Sudoku and Lottery
    Progress part 1

---
+++ sdi-lab10/AguiarGuevarez_FernandoLuis_Lab10Sudoku.js
@@ -0,0 +1,24 @@
+/*
+    Fernando Luis Aguiar Guevarez
+    sdi - Lab 10
+    05/26/15
+    Sudoku and Puzzles
+ */
+
+//alert("JavaScript works!");
+
+//global var
+var userPick;
+var returnedResult;
+
+//function
+function lotteryFunk()
+{
+
+}
+
+//main code
+
+userPick = prompt("Do you want a Quick Pick from the Powerball Numbers or the Florida Lottery Numbers? Write FL for the Florida lottery or write PB for the Powerball Numbers.", "Write in here");
+
+returnedResult = lotteryFunk(userPick);
